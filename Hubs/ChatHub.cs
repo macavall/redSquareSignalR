9cb09cc8a8582767db688073e931627ffa9bcb79
Matthew Cavallo <macavall@microsoft.com>
2024-03-05 19:17:59 -0600

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task UpdateSquarePosition(int newX, int newY)
+        {
+            await Clients.All.SendAsync("UpdateSquarePosition", newX, newY);
+        }
+    }
+}
---
+++ wwwroot/script.js
@@ -0,0 +1,57 @@
+// script.js
+
+"use strict";
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+
+drawSquare();
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("UpdateSquarePosition", function (newX, newY) {
+    x = newX;
+    y = newY;
+    drawSquare();
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.addEventListener("keydown", function (event) {
+    var newX = x;
+    var newY = y;
+    switch (event.key) {
+        case "ArrowUp":
+            newY -= 10;
+            break;
+        case "ArrowDown":
+            newY += 10;
+            break;
+        case "ArrowLeft":
+            newX -= 10;
+            break;
+        case "ArrowRight":
+            newX += 10;
+            break;
+    }
+
+    if (newX !== x || newY !== y) {
+        x = newX;
+        y = newY;
+        drawSquare();
+        connection.invoke("UpdateSquarePosition", newX, newY).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+});
+
+function drawSquare() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "red";
+    ctx.fillRect(x - 25, y - 25, 50, 50);
+}
